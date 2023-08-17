72542a92e0e6ff77f603c2bb33ee30e6f45639ed
natikozel <netanelkozel@gmail.com>
2023-08-17 20:15:54 +0300

    Initial commit

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,46 @@
+import View from './view.js';
+import icons from 'url:../../img/icons.svg';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+  _message = "Recipe was successfully uploaded"
+
+  constructor() {
+    super();
+    this.addHandlerShowWindow();
+    this.addHandlerCloseWindow();
+  }
+
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  addHandlerCloseWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+    window.addEventListener('keydown', e => {
+      if (e.key === 'Escape')
+        this.toggleWindow();
+    });
+  }
+
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function(e) {
+      e.preventDefault()
+      const data = Object.fromEntries([...new FormData(this)]);
+      handler(data);
+    })
+  }
+}
+
+
+export default new AddRecipeView();
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,65 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import View from './view.js';
+import { Fraction } from 'fractional';
+import icons from 'url:../../img/icons.svg';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+
+  _generateMarkUp() {
+
+    const numPages = Math.ceil(this._data.recipes.length / this._data.resultsPerPage);
+    const curPage = this._data.page;
+    if (numPages > 1) {
+      if (curPage === 1) {
+        return`
+        <button data-goto="${curPage+1}" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage+1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>`
+      }
+      if (curPage === numPages) {
+        return `
+         <button data-goto="${curPage-1}" class='btn--inline pagination__btn--prev'>
+           <svg class='search__icon'>
+             <use href='${icons}#icon-arrow-left'></use>
+           </svg>
+           <span>Page ${curPage-1}</span>
+         </button>`;
+      }
+    }
+
+    if (curPage < numPages) {
+      return `
+         <button data-goto="${curPage-1}" class='btn--inline pagination__btn--prev'>
+           <svg class='search__icon'>
+             <use href='${icons}#icon-arrow-left'></use>
+           </svg>
+           <span>Page ${curPage-1}</span>
+         </button>
+        <button data-goto="${curPage+1}" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage+1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>`
+    }
+
+    return '';
+  }
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function(e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+      const nextPage = +btn.dataset.goto;
+      handler(nextPage);
+    })
+  }
+}
+
+export default new PaginationView();
